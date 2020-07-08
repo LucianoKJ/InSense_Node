ecc9e062d6f18f92ae49cf8c692ec6fff309b8a9
svoriagowirs <svoriagowirs@svoriagowirsdeMacBook-Pro.local>
2020-07-08 18:08:29 +0800

    0708

--- routes/orders.js
+++ routes/orders.js
@@ -56,122 +56,138 @@ router.get("/test", async (req, res) => {
   });
 });
 
-
-router.get('/orderdetail/:orderId', async (req, res) => {
-  const orderId = req.params.orderId
+router.get("/orderdetail/:orderId", async (req, res) => {
+  const orderId = req.params.orderId;
   const output = {
     success: false,
-    orderId: orderId
-  }
+    orderId: orderId,
+  };
 
-  const orderSql = "SELECT `coupon`.`couponDiscount`,`OrderTb`.`totalPrice` FROM `OrderTb` INNER JOIN `coupon` ON `OrderTb`.`couponId` = `coupon`.`couponId` WHERE `OrderId` = ?"
+  const orderSql =
+    "SELECT `coupon`.`couponDiscount`,`OrderTb`.`totalPrice` FROM `OrderTb` INNER JOIN `coupon` ON `OrderTb`.`couponId` = `coupon`.`couponId` WHERE `OrderId` = ?";
 
-  const [getOrderInfo] = await db.query(orderSql, [orderId])
-  console.log(getOrderInfo)
-  output.totalPrice = getOrderInfo[0].totalPrice
-  output.couponDiscount = getOrderInfo[0].couponDiscount
+  const [getOrderInfo] = await db.query(orderSql, [orderId]);
+  console.log("getOrderInfo", getOrderInfo);
+  output.totalPrice = getOrderInfo[0].totalPrice;
+  output.couponDiscount = getOrderInfo[0].couponDiscount;
 
-  const deliverySql = "SELECT `userLastName`,`userFirstName`,`userPostCode`,`userCity`,`userDistrict`,`userAddress`,`userPhone` FROM `ordercheckoutpage` WHERE `orderId` = ?"
+  const deliverySql =
+    "SELECT `userLastName`,`userFirstName`,`userPostCode`,`userCity`,`userDistrict`,`userAddress`,`userPhone` FROM `ordercheckoutpage` WHERE `orderId` = ?";
 
-  const [getDeliveryData] = await db.query(deliverySql, [orderId])
-  output.deliveryData = getDeliveryData[0]
+  const [getDeliveryData] = await db.query(deliverySql, [orderId]);
+  output.deliveryData = getDeliveryData[0];
 
-  const creditCardSql = "SELECT `association`,`cdNumber`,`cdMonth`,`cdYear`,`cdHolder`,`billAddressPostCode`,`billAddressCity`,`billAddressDistrict`,`billAddressStreet` FROM `paymentCreditCards` WHERE `orderId` = ?"
+  const creditCardSql =
+    "SELECT `association`,`cdNumber`,`cdMonth`,`cdYear`,`cdHolder`,`billAddressPostCode`,`billAddressCity`,`billAddressDistrict`,`billAddressStreet` FROM `paymentCreditCards` WHERE `orderId` = ?";
 
-  const [getCreditCardData] = await db.query(creditCardSql, [orderId])
-  output.creditCardData = getCreditCardData
+  const [getCreditCardData] = await db.query(creditCardSql, [orderId]);
+  output.creditCardData = getCreditCardData;
 
-  const orderItemSql = "SELECT `orderitemlist`.`itemimg`,`orderitemlist`.`itemName`,`orderitemlist`.`itemPrice`,`orderitemlist`.`quantity`,`orderitemlist`.`itemId`,`items`.`itemSize` FROM `orderitemlist` INNER JOIN `items` ON `orderitemlist`.`itemId` = `items`.`itemId` WHERE `orderitemlist`.`orderId`=?"
-  const [getOrderItem] = await db.query(orderItemSql, [orderId])
-  output.orderItems = getOrderItem
+  const orderItemSql =
+    "SELECT `orderitemlist`.`itemimg`,`orderitemlist`.`itemName`,`orderitemlist`.`itemPrice`,`orderitemlist`.`quantity`,`orderitemlist`.`itemId`,`items`.`itemSize` FROM `orderitemlist` INNER JOIN `items` ON `orderitemlist`.`itemId` = `items`.`itemId` WHERE `orderitemlist`.`orderId`=?";
+  const [getOrderItem] = await db.query(orderItemSql, [orderId]);
+  output.orderItems = getOrderItem;
 
-  output.success = true
-  res.json(output)
+  output.success = true;
+  res.json(output);
+});
 
-})
-
-router.get('/getCreditCardInfo', async (req, res) => {
-  const userId = req.session.userId
+router.get("/getCreditCardInfo", async (req, res) => {
+  const userId = req.session.userId;
   const output = {
     success: false,
-  }
-  const getCreditCardInfoSql = "SELECT `association`,`cdNumber`,`cdMonth`,`cdYear`,`cdHolder`,`billAddressCity`,`billAddressPostCode`,`billAddressDistrict`,`billAddressStreet` FROM `creditcards` WHERE `userId` = ? AND `isDefault` = 1"
+  };
+  const getCreditCardInfoSql =
+    "SELECT `association`,`cdNumber`,`cdMonth`,`cdYear`,`cdHolder`,`billAddressCity`,`billAddressPostCode`,`billAddressDistrict`,`billAddressStreet` FROM `creditcards` WHERE `userId` = ? AND `isDefault` = 1";
 
-  const [response] = await db.query(getCreditCardInfoSql, [userId])
-  response[0].cdMonth = response[0].cdMonth < 10 ? `0${response[0].cdMonth}` : response[0].cdMonth
-  response[0].cdYear = response[0].cdYear < 10 ? `0${response[0].cdYear}` : response[0].cdYear
-  output.creditCardInfo = response[0]
-  output.success = true
-  res.json(output)
-})
+  const [response] = await db.query(getCreditCardInfoSql, [userId]);
+  response[0].cdMonth =
+    response[0].cdMonth < 10 ? `0${response[0].cdMonth}` : response[0].cdMonth;
+  response[0].cdYear =
+    response[0].cdYear < 10 ? `0${response[0].cdYear}` : response[0].cdYear;
+  output.creditCardInfo = response[0];
+  output.success = true;
+  res.json(output);
+});
 
 router.post("/orderList", async (req, res) => {
   const output = {
     success: false,
-    orderId: ''
-  }
-
+    orderId: "",
+  };
 
   //新增訂單
   const orderDetaildata = {
     userId: req.body.paymentdata.userId,
-    orderStatus: '處理中',
-    totalPrice: req.body.selectCartTotal
-  }
+    orderStatus: "處理中",
+    totalPrice: req.body.selectCartTotal,
+  };
 
   //找尋付款時有無優惠券
   if (req.body.selectCouponCode) {
-    const couponSQL = 'SELECT `couponId`,`couponDiscount` FROM `coupon` WHERE `couponCode` = ?'
-    const [couponDiscount] = await db.query(couponSQL, [req.body.selectCouponCode])
-    orderDetaildata.couponId = couponDiscount[0].couponId
-    orderDetaildata.totalPrice = orderDetaildata.totalPrice - couponDiscount[0].couponDiscount
+    const couponSQL =
+      "SELECT `couponId`,`couponDiscount` FROM `coupon` WHERE `couponCode` = ?";
+    const [couponDiscount] = await db.query(couponSQL, [
+      req.body.selectCouponCode,
+    ]);
+    orderDetaildata.couponId = couponDiscount[0].couponId;
+    orderDetaildata.totalPrice =
+      orderDetaildata.totalPrice - couponDiscount[0].couponDiscount;
   }
 
-  const orderSQL = 'INSERT INTO `OrderTb` SET ?'
-  const [orderDetail] = await db.query(orderSQL, [orderDetaildata])
-
+  const orderSQL = "INSERT INTO `OrderTb` SET ?";
+  const [orderDetail] = await db.query(orderSQL, [orderDetaildata]);
 
   //增加orderId
   if (orderDetail.affectedRows > 0) {
-    const sqlOrderId = "UPDATE `OrderTb` SET `orderId` = ? WHERE id = ? "
-    const insertOrderId = orderDetail.insertId.toString()
+    const sqlOrderId = "UPDATE `OrderTb` SET `orderId` = ? WHERE id = ? ";
+    const insertOrderId = orderDetail.insertId.toString();
     //取得orderId
-    output.orderId = makeFormatedId(6, "O_", insertOrderId)
-    await db.query(sqlOrderId, [output.orderId, insertOrderId])
+    output.orderId = makeFormatedId(6, "O_", insertOrderId);
+    await db.query(sqlOrderId, [output.orderId, insertOrderId]);
   }
 
-
   const creditcardSQL = "INSERT INTO paymentCreditCards SET ?";
-  const orderCreditCardData = req.body.paymentdata
-  orderCreditCardData.orderId = output.orderId
-  const [creditcarddata] = await db.query(creditcardSQL, [req.body.paymentdata,]);
+  const orderCreditCardData = req.body.paymentdata;
+  orderCreditCardData.orderId = output.orderId;
+  const [creditcarddata] = await db.query(creditcardSQL, [
+    req.body.paymentdata,
+  ]);
 
-  //增加寄送資訊  
+  //增加寄送資訊
   const orderDeliverySQL = "INSERT INTO ordercheckoutpage SET ?";
-  const orderDeliveryData = req.body.orderDelivery.data
-  orderDeliveryData.orderId = output.orderId
-  const [orderDelivery] = await db.query(orderDeliverySQL, [orderDeliveryData,]);
+  const orderDeliveryData = req.body.orderDelivery.data;
+  orderDeliveryData.orderId = output.orderId;
+  const [orderDelivery] = await db.query(orderDeliverySQL, [orderDeliveryData]);
   //增加deliveryId
   if (orderDelivery.affectedRows > 0) {
-    const sqlDeliveryId = "UPDATE `ordercheckoutpage` SET `deliveryId` = ? WHERE id = ? "
-    const insertDeliveryId = orderDelivery.insertId.toString()
+    const sqlDeliveryId =
+      "UPDATE `ordercheckoutpage` SET `deliveryId` = ? WHERE id = ? ";
+    const insertDeliveryId = orderDelivery.insertId.toString();
     //增加deliveryId
-    output.deliveryId = makeFormatedId(6, "D_", insertDeliveryId)
-    await db.query(sqlDeliveryId, [output.deliveryId, insertDeliveryId])
+    output.deliveryId = makeFormatedId(6, "D_", insertDeliveryId);
+    await db.query(sqlDeliveryId, [output.deliveryId, insertDeliveryId]);
   }
 
   //增加購買的商品列表
-  const orderItemSQL = "INSERT INTO orderitemlist (`itemId`,`itemName`,`itemPrice`,`itemimg`,`quantity`,`orderId`,`userId`) VALUES (?,?,?,?,?,?,?)";
+  const orderItemSQL =
+    "INSERT INTO orderitemlist (`itemId`,`itemName`,`itemPrice`,`itemimg`,`quantity`,`orderId`,`userId`) VALUES (?,?,?,?,?,?,?)";
   req.body.selectCartItems.forEach((el) => {
-    el.orderId = output.orderId
-    el.userId = orderDetaildata.userId
-    db.query(orderItemSQL, [el.itemId, el.itemName, el.itemPrice, el.itemimg, el.quantity, el.orderId, el.userId]);
+    el.orderId = output.orderId;
+    el.userId = orderDetaildata.userId;
+    db.query(orderItemSQL, [
+      el.itemId,
+      el.itemName,
+      el.itemPrice,
+      el.itemimg,
+      el.quantity,
+      el.orderId,
+      el.userId,
+    ]);
   });
-  output.success = true
+  output.success = true;
 
   res.json(output);
 });
-
-
 
 module.exports = router;

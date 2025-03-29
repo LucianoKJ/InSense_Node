d6dca08f563d543fe33e24fa06a9326feae31981
Luciano <lucianokuanjung@gmail.com>
2025-03-29 09:10:36 +0800

    modify database name

--- routes/db_connect.js
+++ routes/db_connect.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: "localhost",
     user: "test",
     password: "test",
-    database: "InSense",
+    database: "Insense",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
